be937fb13f589ec22c80a6e4e63d840af9e8a53a
wani3000 <54127248+wani3000@users.noreply.github.com>
2025-07-25 00:58:42 +0900

    구글 애드센스 자동광고 스크립트 <head>에 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,8 @@ export default function RootLayout({
           as="style"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
         />
+        {/* Google AdSense Auto Ads */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6858835884991650" crossOrigin="anonymous"></script>
       </head>
       <body className="font-pretendard">{children}</body>
     </html>
