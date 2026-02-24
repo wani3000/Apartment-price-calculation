c5d9b2c37ba8a543a10184c5e071bc1f67c3c6b0
wani3000 <54127248+wani3000@users.noreply.github.com>
2026-02-24 18:29:59 +0900

    Fix latest apartments API import path

--- api/region/latest-apartments.js
+++ api/region/latest-apartments.js
@@ -1,4 +1,4 @@
-import { latestApartments } from "../../_lib/recommend-core.js";
+import { latestApartments } from "../_lib/recommend-core.js";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
